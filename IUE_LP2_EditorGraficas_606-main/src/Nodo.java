507f40a79bd94b6bf17d891baa1f3d5328a3f119
Daniriut06 <daniel.luna.mono@gmail.com>
2025-04-09 10:59:35 -0500

    a

---
+++ IUE_LP2_EditorGraficas_606-main/src/Nodo.java
@@ -0,0 +1,40 @@
+import java.awt.Color;
+
+public class Nodo {
+    private Trazo trazo;
+    private Color color;
+
+    public Nodo siguiente;
+
+    
+
+    public Trazo getTrazo() {
+        return trazo;
+    }
+
+
+
+    public void setTrazo(Trazo trazo) {
+        this.trazo = trazo;
+    }
+
+
+
+    public Color getColor() {
+        return color;
+    }
+
+
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+
+
+    public Nodo(Trazo trazo, Color color) {
+        this.trazo = trazo;
+        this.color = color;
+    }
+
+}
