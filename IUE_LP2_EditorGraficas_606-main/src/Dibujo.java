54f1047b340cc019276a7cce05fd0822f9e0f4c8
Daniriut06 <daniel.luna.mono@gmail.com>
2025-04-21 11:14:37 -0500

    se hace el guardado y abrir archivo

--- IUE_LP2_EditorGraficas_606-main/src/Dibujo.java
+++ IUE_LP2_EditorGraficas_606-main/src/Dibujo.java
@@ -1,7 +1,12 @@
 import java.awt.Graphics;
+import java.util.List;
 import java.awt.Color;
 
 import javax.swing.JPanel;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+
+import entidades.TrazoDTO;
 
 public class Dibujo {
     private Nodo cabeza;
@@ -24,16 +29,68 @@ public class Dibujo {
         nodo.siguiente = null;
     }
 
-    public void dibujar(JPanel pnl){
-        limpiarPanel(pnl);
-        //obtener el objeto graficador
-        Graphics g = pnl.getGraphics();
-        //recorrer lista
+    public int getLongitud() {
+        int totalNodos = 0;
+        // no sabemos cuantos nodos tenemos entonces NO usamos for, sino un while
         Nodo actual = cabeza;
-        while (actual != null){
+        while (actual != null) {
+            totalNodos += 1;
+            actual = actual.siguiente;
+
+        }
+
+        return totalNodos;
+    }
+
+    public void dibujar(JPanel pnl) {
+        limpiarPanel(pnl);
+        // obtener el objeto graficador
+        Graphics g = pnl.getGraphics();
+        // recorrer lista
+        Nodo actual = cabeza;
+        while (actual != null) {
             actual.getTrazo().dibujar(g, actual.getColor());
             actual = actual.siguiente;
         }
+    }
+
+    public boolean guardarJSON(String nombreArchivo){
+        TrazoDTO[] trazos = new TrazoDTO[getLongitud()];
+        Nodo actual = cabeza;
+        int fila = 0;
+        while (actual != null){
+            trazos[fila] = actual.toDTO();
+            fila++;
+            actual = actual.siguiente;
+        }
+        return Archivo.guardarJson(nombreArchivo, trazos);
+
+    }
+
+    public void desdeJSON (String nombreArchivo){
+        List <TrazoDTO> trazos = Archivo.leerJson(nombreArchivo, new TypeReference<List<TrazoDTO>>() {});
+        cabeza = null;
+        for (TrazoDTO dto :trazos){
+            Trazo trazo = null;
+            switch (TipoTrazo.valueOf(dto.getTipo())) {
+
+                case LINEA:
+                    trazo = new Linea(dto.getX1(),dto.getY1(),dto.getX2(),dto.getY2());
+                    break;
+                case RECTANGULO:    
+                    trazo = new Rectangulo(dto.getX1(),dto.getY1(),dto.getX2(),dto.getY2());
+                    break;
+                case OVALO:
+                    trazo = new Ovalo(dto.getX1(),dto.getY1(),dto.getX2(),dto.getY2());
+                    break;
+
+                
+            }
+            if (trazo != null){
+                agregar(new Nodo(trazo, new Color(dto.getRed(), dto.getGreen(), dto.getBlue())));
+            }
+        }
+        
     }
 
     // ********** Metodos Estaticos **********
--- IUE_LP2_EditorGraficas_606-main/src/Nodo.java
+++ IUE_LP2_EditorGraficas_606-main/src/Nodo.java
@@ -1,40 +1,42 @@
 import java.awt.Color;
 
+
+
+import entidades.TrazoDTO;
+
 public class Nodo {
     private Trazo trazo;
     private Color color;
 
     public Nodo siguiente;
 
-    
-
     public Trazo getTrazo() {
         return trazo;
     }
-
-
 
     public void setTrazo(Trazo trazo) {
         this.trazo = trazo;
     }
 
-
-
     public Color getColor() {
         return color;
     }
-
-
 
     public void setColor(Color color) {
         this.color = color;
     }
 
-
-
     public Nodo(Trazo trazo, Color color) {
         this.trazo = trazo;
         this.color = color;
+    }
+
+    public TrazoDTO toDTO() {
+
+        return new TrazoDTO(trazo instanceof Linea ? "LINEA" : trazo instanceof Rectangulo ? "RECTANGULO" : "OVALO",
+                trazo.getX1(), trazo.getY1(), trazo.getX2(), trazo.getY2(),
+                color.getRed(), color.getGreen(), color.getBlue());
+
     }
 
 }
